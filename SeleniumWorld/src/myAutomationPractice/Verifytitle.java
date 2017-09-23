eacbcca376a1d28c67eaf8a6c08228abe330069c
reach <reach@DESKTOP-3O31J9L.gateway>
2017-09-23 17:01:59 +1000

    check in 14-i want to kick him on his back

--- SeleniumWorld/src/myAutomationPractice/Verifytitle.java
+++ SeleniumWorld/src/myAutomationPractice/Verifytitle.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.remote.server.handler.WebDriverHandler;
   public class Verifytitle {
 
 	public static void main(String[] args) {
-	WebDriver driver=new InternetExplorerDriver();
+	 WebDriver driver=new InternetExplorerDriver();
     driver.get("https://www.google.com.au/?gfe_rd=cr&ei=LgA1Wey0FdLu8weFjJDwBg&gws_rd=ssl");
 	 String Title = driver.getTitle();
 	 if (Title.equals("Google"))
